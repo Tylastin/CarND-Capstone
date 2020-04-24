22ba58905099d137fc994497ec77c19444ba8d1f
root <fflores321@gmail.com>
2020-04-24 01:58:25 +0000

    feat: Add traffic sign classifier (SSD)

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-This is the project repo for the final project of the Udacity Self-Driving Car Nanodegree: Programming a Real Self-Driving Car. For more information about the project, see the project introduction [here](https://classroom.udacity.com/nanodegrees/nd013/parts/6047fe34-d93c-4f50-8336-b70ef10cb4b2/modules/e1a23b06-329a-4684-a717-ad476f0d8dff/lessons/462c933d-9f24-42d3-8bdc-a08a5fc866e4/concepts/5ab4b122-83e6-436d-850f-9f4d26627fd9).
+# Udacity Self-Driving Car Nanodegree: Programming a Real Self-Driving Car
+This is a rospy based project incorporating the primary subsystems of a self driving car: perception, planning, and control. The ROS based systems is able to drive a car autonomously under controlled conditions. 
+
+Authors:
+Name: Fabio Flores
+email: fflores321@gmail.com
+
+![alt text](./imgs/architecture_image.png "System Architecture")
 
 Please use **one** of the two installation options, either native **or** docker installation.
 
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -5,14 +5,16 @@ from geometry_msgs.msg import PoseStamped, Pose
 from styx_msgs.msg import TrafficLightArray, TrafficLight
 from styx_msgs.msg import Lane
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge   
+from cv_bridge import CvBridge
 from light_classification.tl_classifier import TLClassifier
+
 import tf
 import cv2
 import yaml
+import math
 from scipy.spatial import KDTree
 
-STATE_COUNT_THRESHOLD = 3
+STATE_COUNT_THRESHOLD = 2
 
 class TLDetector(object):
     def __init__(self):
@@ -40,11 +42,11 @@ class TLDetector(object):
 
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
-
+        self.is_site = self.config["is_site"]
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
+        self.light_classifier = TLClassifier(self.is_site)
         self.listener = tf.TransformListener()
 
         self.state = TrafficLight.UNKNOWN
@@ -121,15 +123,15 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        return light.state
-#         if(not self.has_image):
-#             self.prev_light_loc = None
-#             return False
+#         return light.state
+        if(not self.has_image):
+            self.prev_light_loc = None
+            return False
 
-#         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-#         #Get classification
-#         return self.light_classifier.get_classification(cv_image)
+        #Get classification
+        return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
